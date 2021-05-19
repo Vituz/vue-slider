85b27f390b0b78686580727f63d3bd9ee34beae3
Vituz <vtbattaglia@gmail.com>
2021-05-19 03:15:55 +0200

    Add slider dot

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,49 @@ const app = new Vue({
     el: '#root',
 
     data:{
+        counter: 0,
 
+        images:[
+            './assets/img/paesaggio01.jpg',
+            './assets/img/paesaggio02.jpg',
+            './assets/img/paesaggio03.jpg',
+            './assets/img/paesaggio04.jpg',
+            './assets/img/paesaggio05.jpg',
+        ],
+
+        bgBlue: 'blue'
     },
 
     methods:{
+        prev(){
+            console.log("Hai cliccato tu previous");
+
+            clearInterval(this.setInterval); 
+
+            if(this.counter == 0){
+                return this.counter = this.images.length-1;
+            }
+             return this.counter--;
+        },
+
+        next(){
+            console.log("Hai cliccato su next");
+
+            clearInterval(this.setInterval);
+
+            if(this.counter == this.images.length -1){
+               return this.counter = 0;
+            }
+             return this.counter++;
+        },
+
+        selectImg(){
+            console.log(this.counter);
+        }
 
     },
-
-    mounted(){
-        
-    }
+    
+    mounted: function(){
+        setInterval(this.next, 3000);
+    },
 });
